a5c8fd1129a9ea6c328eab2d1416ec4204c051df
Fernando Viana <fernandovianadossantos2001@gmail.com>
2023-04-16 21:04:54 -0400

    finalizando projeto e contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
-console.log("olá mundo");
+AOS.init();
+
+const dataEvento = new Date("Jul 6, 2023 19:00:00");
+const timeStampDoEvento = dataEvento.getTime();
+
+const contaAsHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minsAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const secAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minsAteOEvento}m ${secAteOEvento}s`;
+
+    if(distanciaAteOEvento < 0){
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = `O Evento Expirou!`;
+    }
+
+})
